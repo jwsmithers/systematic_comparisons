9e739c81600b8a4eea19d15967cc621f383b6002
jwsmith <joshua.wyatt.smith@cern.ch>
2018-04-05 00:20:42 +0200

    large improvements

--- compare_histos.py
+++ compare_histos.py
@@ -1,6 +1,7 @@
 from ROOT import TCanvas, TPad, TFile, TPaveLabel, TPaveText,TLegend, TF1
 from ROOT import gROOT
 gROOT.SetBatch(True)
+import sys
 
 from difflib import SequenceMatcher
 def similar(a, b):
@@ -11,7 +12,10 @@ ejets_root_file="ttgamma_SR1_ejets_2018_03_event_ELD_MVA_ejets_histos.root"
 mujets_path="/afs/cern.ch/work/j/jwsmith/ttgammaPlottingPipeline/build/mujets_fullFit_merged_C_On/build/SR1_ttgamma_SR1_mujets_2018_03/ttgamma_SR1_mujets_2018_03/Histograms/"
 mujets_root_file="ttgamma_SR1_mujets_2018_03_event_ELD_MVA_mujets_histos.root"
 
-channels=["mujets","ejets"]
+singlelepton_path="/afs/cern.ch/work/j/jwsmith/ttgammaPlottingPipeline/build/singlelepton_fullFit_merged_C_On/build/SR1_ejets_mujets_merged/ejets_mujets_merged/Histograms/"
+singlelepton_root_file="ejets_mujets_merged_event_ELD_MVA_ejets_histos.root"
+
+channels=["singlelepton","ejets","mujets"]
 
 g=globals()
 for i in channels:
@@ -19,8 +23,7 @@ for i in channels:
   g[i+"_up"] = []
   g[i+"_down"] = []
 
-
-def create_lists(channel,path):
+def create_lists(channel,path,process):
   histograms=[]
   f = TFile.Open(path, 'read')
   dirlist = f.GetListOfKeys()
@@ -35,130 +38,203 @@ def create_lists(channel,path):
     key = iter.Next()
 
   for i in histograms:
-    if "_ttphoton_regBin" in i:
-      g[channel+"_nominal"].append(i)
-    if "Up_regBin" in i:
-      g[channel+"_up"].append(i)
-    if "Down_regBin" in i:
-      g[channel+"_down"].append(i)
+    if "Data" in i: 
+      continue
+    prefixes=["event_ELD_MVA_ejets_"+process,"event_ELD_MVA_mujets_"+process]
+    if any(proc in i for proc in prefixes):
+      i_split = i.split("_")
+      if i_split[-1]==process:
+        g[channel+"_nominal"].append(i)
+      elif i_split[-1]=="Up":
+        g[channel+"_up"].append(i)
+      elif i_split[-1]=="Down":
+        g[channel+"_down"].append(i)
+
   f.Close()
 
-create_lists("ejets", ejets_path+ejets_root_file)
-create_lists("mujets",mujets_path+mujets_root_file)
-
-
-for hist_int in range(0,len(ejets_up)):
-  f_ejets = TFile.Open(ejets_path+ejets_root_file, 'read')
-  f_mujets = TFile.Open(mujets_path+mujets_root_file, 'read')
-  c1 = TCanvas( 'c1', 'Histogram Drawing Options', 0,0,800,800 )
-  pad1 = TPad("pad1", "pad1", 0, 0.3, 1, 1.0)
-  pad1.SetBottomMargin(0)  # joins upper and lower plot
-  pad1.Draw()
-  pad2 = TPad("pad2", "pad2", 0, 0.05, 1, 0.3)
-  pad2.SetTopMargin(0.0)  # joins upper and lower plot
-  pad2.SetBottomMargin(0.2)
-  pad2.Draw()
-  pad1.cd()
-
-  hist_name_ejets=ejets_up[hist_int]
-  hist_name_mujets=mujets_up[hist_int]
-
-  sim = similar(hist_name_ejets,hist_name_mujets)
-  if sim < 0.9:
-    print hist_name_ejets," and ", hist_name_mujets, " are different!"
-
-  nom_h_ejets = f_ejets.Get(ejets_nominal[0])
-  up_h_ejets = f_ejets.Get(ejets_up[hist_int])
-  down_h_ejets = f_ejets.Get(ejets_down[hist_int])
-  nom_h_mujets = f_mujets.Get(mujets_nominal[0])
-  up_h_mujets = f_mujets.Get(mujets_up[hist_int])
-  down_h_mujets = f_mujets.Get(mujets_down[hist_int])
-
-
-  for i in channels:
-    y1 = g["nom_h_"+i].GetYaxis()
-    y1.SetTitle("Events")
-    x1 = g["nom_h_"+i].GetXaxis()
-    x1.SetTitle("event level descriminator")
-
-    g["nom_h_"+i].Draw("same")
-    g["up_h_"+i].Draw("same")
-    g["down_h_"+i].Draw("same")
-
-  up_h_ejets.SetMarkerColor(632)
-  up_h_ejets.SetLineColor(632)
-  down_h_ejets.SetMarkerColor(857)
-  down_h_ejets.SetLineColor(857)
-
-  nom_h_mujets.SetMarkerStyle(4)
-  up_h_mujets.SetMarkerColor(632)
-  up_h_mujets.SetLineColor(632)
-  up_h_mujets.SetMarkerStyle(4)
-  down_h_mujets.SetMarkerColor(857)
-  down_h_mujets.SetLineColor(857)
-  down_h_mujets.SetMarkerStyle(4)
-
-
-  legend = TLegend(0.15,0.7,0.7,0.85);
-  for i in channels:
-    legend.AddEntry(g["nom_h_"+i],g[i+"_nominal"][0]+" nominal","lep");
-    legend.AddEntry(g["up_h_"+i],g[i+"_up"][0]+" up","lep");
-    legend.AddEntry(g["down_h_"+i],g[i+"_down"][0]+" down","lep");
-
-
-  legend.SetBorderSize(0)
-  legend.SetTextSize(0.015)
-  legend.Draw();
-
-  c1.Update()
-
-  #### ratio
-  pad2.cd()
-  ratio1 = nom_h_ejets.Clone("ratio")
-  ratio1.SetMinimum(0)
-  ratio1.SetMaximum(2)
+def make_ratio(nom,up,down):
+  ratio1 = up.Clone("ratio")
   ratio1.Sumw2()
   ratio1.SetStats(0)
-  ratio1.Divide(up_h_ejets)
+  ratio1.Add(nom,-1)
+  ratio1.Divide(nom)
+  ratio1.Scale(100)
   ratio1.SetTitle("")
   y = ratio1.GetYaxis()
   y.SetTitle("#frac{Syst-Nom.}{Nom.} [%]")
   y.SetNdivisions(505)
-  #y.SetTitleSize(20)
-  #y.SetTitleFont(43)
-  y.SetTitleOffset(1.55)
-  #y.SetLabelFont(43)
-  #y.SetLabelSize(15)
+  y.SetTitleOffset(2.3)
+  y.SetTitleSize(15)
   x = ratio1.GetXaxis()
   x.SetTitle("event level descriminator")
-  #x.SetTitleSize(20)
-  #x.SetTitleFont(43)
   x.SetTitleOffset(3.2)
-  #x.SetLabelFont(43)
-  #x.SetLabelSize(15)
   ratio1.SetLineColor(632)
-  #ratio1.SetMarkerStyle(20)
+  ratio1.SetFillColor(632)
   ratio1.SetMarkerColor(632)
-  ratio1.Draw("")
-
-  ratio2 = nom_h_ejets.Clone("ratio")
-  ratio2.Divide(down_h_ejets)
+  ratio1.SetMarkerStyle(0)
+  ratio1.Draw("HIST X")
+  ratio2 = down.Clone("ratio")
+  ratio2.Sumw2()
+  ratio2.Add(nom,-1)
+  ratio2.Divide(nom)
+  ratio2.Scale(100)
   ratio2.SetLineColor(857)
-  #ratio2.SetMarkerStyle(20)
+  ratio2.SetFillColor(857)
   ratio2.SetMarkerColor(857)
-  ratio2.Draw("same")
-  
-
-  line = TF1("fa1","1",-1000,1000);
+  ratio2.SetMarkerStyle(0)
+  ratio2.Draw("same HIST X")
+  line = TF1("fa1","0",-1000,1000);
+  line.SetLineColor(1);
   line.Draw("same")
-  #line.SetLineColor(632);
+  ymax = abs(ratio1.GetMaximum());
+  ymin = abs(ratio2.GetMaximum());
+  Range=[ymax,ymin]
+  ratio1.SetMinimum(-max(Range)-0.5)
+  ratio1.SetMaximum(max(Range)+0.5)
+  ratio2.SetMinimum(-max(Range)-0.5)
+  ratio2.SetMaximum(max(Range)+0.5)
+  return ratio1, ratio2, line
 
-
-  c1.SaveAs("histos/"+hist_name_ejets+".pdf")
-  del c1
-  for i in channels:
-    del g["nom_h_"+i]
-    del g["up_h_"+i]
-    del g["down_h_"+i]
+def plot_histos():
+  for hist_int in range(0,len(ejets_up)):
+    f_ejets = TFile.Open(ejets_path+ejets_root_file, 'read')
+    f_mujets = TFile.Open(mujets_path+mujets_root_file, 'read')
+    f_SL = TFile.Open(singlelepton_path+singlelepton_root_file, 'read')
+    c1 = TCanvas( 'c1', 'Histogram Drawing Options', 0,0,800,800 )
+    #c1.SetBottomMargin(0);
+    pad1 = TPad("pad1", "pad1", 0, 0.5, 1, 1.0)
+    pad1.SetBottomMargin(0)  # joins upper and lower plot
+    pad1.Draw()
+    pad2 = TPad("pad2", "pad2", 0, 0.35, 1, 0.5)
+    pad2.SetTopMargin(0.0)  # joins upper and lower plot
+    pad2.SetBottomMargin(0)
+    pad2.Draw()
+    pad3 = TPad("pad3", "pad3", 0, 0.20, 1, 0.35)
+    pad3.SetTopMargin(0.0)  # joins upper and lower plot
+    pad3.SetBottomMargin(0)
+    pad3.Draw()
+    pad4 = TPad("pad4", "pad4", 0, 0.05, 1, 0.20)
+    pad4.SetTopMargin(0.0)  # joins upper and lower plot
+    pad4.SetBottomMargin(0.05)
+    pad4.Draw()
+    pad1.cd()
   
+    hist_name_ejets=ejets_up[hist_int]
+    hist_name_mujets=mujets_up[hist_int]
+    hist_name_singlelepton=singlelepton_up[hist_int]
+  
+    sim = similar(hist_name_ejets,hist_name_mujets)
+    if "ttphoton_PDF" not in hist_name_ejets:
+      if sim < 0.9:
+        print hist_name_ejets," and ", hist_name_mujets, " are different!"
+        sys.exit()
+      sim2 = similar(hist_name_singlelepton,hist_name_ejets)
+      # These should be identical
+      if sim2 != 1.0:
+        print hist_name_singlelepton," and ", hist_name_ejets, " are different!"
+        sys.exit()
+
+    g["nom_h_ejets"] = f_ejets.Get(ejets_nominal[0])
+    g["up_h_ejets"] = f_ejets.Get(ejets_up[hist_int])
+    g["down_h_ejets"] = f_ejets.Get(ejets_down[hist_int])
+    g["nom_h_mujets"] = f_mujets.Get(mujets_nominal[0])
+    g["up_h_mujets"] = f_mujets.Get(mujets_up[hist_int])
+    g["down_h_mujets"] = f_mujets.Get(mujets_down[hist_int])
+
+    g["nom_h_singlelepton"] = f_SL.Get(singlelepton_nominal[0])
+    g["up_h_singlelepton"] = f_SL.Get(singlelepton_up[hist_int])
+    g["down_h_singlelepton"] = f_SL.Get(singlelepton_down[hist_int])
+  
+  
+    for i in channels:
+      y1 = g["up_h_"+i].GetYaxis()
+      y1.SetTitle("Events")
+      y1.SetTitleOffset(2)
+      x1 = g["up_h_"+i].GetXaxis()
+      x1.SetTitle("event level descriminator")
+      g["up_h_"+i].SetTitle("")
+      x1.SetTitleOffset(1.5)
+      g["up_h_"+i].Draw("same")
+      g["down_h_"+i].Draw("same")
+      g["nom_h_"+i].Draw("same")
+  
+    nom_h_ejets.SetMarkerColor(1)
+    nom_h_ejets.SetLineColor(1)
+    up_h_ejets.SetMarkerColor(632)
+    up_h_ejets.SetLineColor(632)
+    down_h_ejets.SetMarkerColor(857)
+    down_h_ejets.SetLineColor(857)
+  
+    nom_h_mujets.SetMarkerStyle(4)
+    nom_h_mujets.SetMarkerColor(1)
+    nom_h_mujets.SetLineColor(1)
+    up_h_mujets.SetMarkerColor(632)
+    up_h_mujets.SetLineColor(632)
+    up_h_mujets.SetMarkerStyle(4)
+    down_h_mujets.SetMarkerColor(857)
+    down_h_mujets.SetLineColor(857)
+    down_h_mujets.SetMarkerStyle(4)
+  
+    nom_h_singlelepton.SetMarkerStyle(26)
+    nom_h_singlelepton.SetMarkerColor(1)
+    nom_h_singlelepton.SetLineColor(1)
+    up_h_singlelepton.SetMarkerColor(632)
+    up_h_singlelepton.SetLineColor(632)
+    up_h_singlelepton.SetMarkerStyle(26)
+    down_h_singlelepton.SetMarkerColor(857)
+    down_h_singlelepton.SetLineColor(857)
+    down_h_singlelepton.SetMarkerStyle(26)
+  
+    legend = TLegend(0.15,0.4,0.55,0.85);
+  
+    for i in channels:
+      if i=="singlelepton":
+        legend.AddEntry(g["nom_h_"+i],g[i+"_nominal"][0].replace("event_ELD_MVA_","").replace("ejets","SL").replace("_regBin","")+" nominal","lep");
+        legend.AddEntry(g["up_h_"+i],g[i+"_up"][hist_int].replace("event_ELD_MVA_","").replace("ejets","SL").replace("_regBin",""),"lep");
+        legend.AddEntry(g["down_h_"+i],g[i+"_down"][hist_int].replace("event_ELD_MVA_","").replace("ejets","SL").replace("_regBin",""),"lep");
+      else:
+        legend.AddEntry(g["nom_h_"+i],g[i+"_nominal"][0].replace("event_ELD_MVA_","").replace("_regBin","")+" nominal","lep");
+        legend.AddEntry(g["up_h_"+i],g[i+"_up"][hist_int].replace("event_ELD_MVA_","").replace("_regBin",""),"lep");
+        legend.AddEntry(g["down_h_"+i],g[i+"_down"][hist_int].replace("event_ELD_MVA_","").replace("_regBin",""),"lep");
+  
+  
+    legend.SetBorderSize(0)
+    legend.SetTextSize(0.03)
+    legend.Draw();
+  
+    c1.Update()
+  
+    pad2.cd()
+    r1=make_ratio(nom_h_ejets,up_h_ejets,down_h_ejets)
+    r1[0].Draw("same")
+    r1[1].Draw("same")
+    r1[2].Draw("same")
+    pad3.cd()
+    r2=make_ratio(nom_h_mujets,up_h_mujets,down_h_mujets)
+    r2[0].Draw("same")
+    r2[1].Draw("same")
+    r2[2].Draw("same")
+    pad4.cd()
+    r3=make_ratio(nom_h_singlelepton,up_h_singlelepton,down_h_singlelepton)
+    r3[0].Draw("same")
+    r3[1].Draw("same")
+    r3[2].Draw("same")
+  
+    c1.SaveAs("histos/"+hist_name_ejets+".pdf")
+    del c1
+    for i in channels:
+      del g["nom_h_"+i]
+      del g["up_h_"+i]
+      del g["down_h_"+i]
+    
+
+def main(Process):
+  create_lists("ejets", ejets_path+ejets_root_file,Process)
+  create_lists("mujets",mujets_path+mujets_root_file,Process)
+  create_lists("singlelepton",singlelepton_path+singlelepton_root_file,Process)
+  assert len(ejets_up)==len(ejets_down)==len(mujets_up)==\
+    len(mujets_down)==len(singlelepton_up)==len(singlelepton_down)
+  plot_histos()
+  
+main("ttphoton")
 
